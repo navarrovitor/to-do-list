9bb132ad3f8be095a866a6b5ebb9d7a0c9a9b9c4
Vitor Navarro <vitor.san.nav.nexus@gmail.com>
2023-06-12 17:24:55 -0300

    Initiate modal

--- app/javascript/controllers/list_modal_controller.js
+++ app/javascript/controllers/list_modal_controller.js
@@ -2,6 +2,33 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="list-modal"
 export default class extends Controller {
+  static targets = ["modal", "title", "author", "items"]
+
   connect() {
+    console.log("List modal controller connected")
+  }
+
+  showModal(event) {
+    const listId = event.currentTarget.dataset.listId
+
+    console.log(listId);
+    fetch(`/lists/${listId}`)
+      .then((response) => response.json())
+      .then((data) => {
+        this.titleTarget.textContent = data.name
+        this.authorTarget.textContent = data.author
+
+        let itemsHtml = ""
+        data.items.forEach((item) => {
+          itemsHtml += `<li>${item.title}</li>`
+        })
+        this.itemsTarget.innerHTML = itemsHtml
+
+        this.modalTarget.classList.add("active")
+      })
+  }
+
+  hideModal() {
+    this.modalTarget.classList.remove("active")
   }
 }
